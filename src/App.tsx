c7da9226f9818b80711accf3c2c4cb64729e144d
Priyank Patel <priyank.patel@stackspace.ca>
2020-07-07 11:59:38 -0400

    chore: update example

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+import { Plugins } from '@capacitor/core';
+
 import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
@@ -23,15 +25,31 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+const { TextToSpeech } = Plugins;
+
+const App: React.FC = () => {
+  useEffect(() => {
+    TextToSpeech.speak({
+      text: "This is a sample text.",
+      locale: "en_US",
+      speechRate: 0.55,
+      pitchRate: 1,
+      volume: 1,
+      voice: 10,
+      category: "ambient",
+    });
+  });
+
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route path="/home" component={Home} exact={true} />
+          <Route exact path="/" render={() => <Redirect to="/home" />} />
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
+  )
+};
 
 export default App;
